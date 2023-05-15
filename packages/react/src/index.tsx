89103e87d5f98d9847afb2a05161d3bda5a956d6
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-05-15 16:12:14 -0300

    documentação de cores

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -3,29 +3,29 @@ import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
-    fontFamily: '$default',
-    backgroundColor: '$ignite300',
-    borderRadius: '$sm',
-    border: 0,
-    fontWeight: 'bold',
-    color: '$white',
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
 
-    variants: {
-        size: {
-            small: {
-                fontSize: 14,
-                padding: '$2 $4',
-            },
-            big: {
-                fontSize: 16,
-                padding: '$3 $6',
-            },
-        },
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
     },
+  },
 
-    defaultVariants: {
-        size: 'small',
-    },
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
 export type ButtonProps = ComponentProps<typeof Button>
